3442299ce91d0d9e46c61fb1c2d4dbcda5241d2a
NYAINA_FITIAVANA <nfitahiantsoa@ingenosya.mg>
2025-02-11 15:34:20 +0300

    init project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,8 @@
-import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+import React from "react";
+import type {Metadata} from "next";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {ClerkProvider} from "@clerk/nextjs";
+import {ThemeProvider} from "@/components/providers/ThemeProvider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -23,12 +15,19 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
-    </html>
+      <ClerkProvider>
+        <html lang="en">
+          <body
+          >
+            <ThemeProvider
+                attribute="class"
+                defaultTheme="system"
+                enableSystem
+            >
+               {children}
+            </ThemeProvider>
+          </body>
+        </html>
+      </ClerkProvider>
   );
 }
